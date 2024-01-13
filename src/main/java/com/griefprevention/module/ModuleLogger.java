865d6f4f46dc5cf3e111a6df13ca8377490b5bb1
Jikoo <jikoo.games@gmail.com>
2024-01-13 13:21:52 -0500

    Clean up logging a little
    This is a bit of a hack, but modifying the configuration to not log the logger name for modules similarly to how plugin logger names are not logged is kind of a hassle. The vast majority of users will not be using custom log4j configs (and don't know how) which is also why Bukkit plugins traditionally have their own debug flags rather than letting the logger deal with it.

--- src/main/java/com/griefprevention/module/ModuleLogger.java
+++ src/main/java/com/griefprevention/module/ModuleLogger.java
@@ -7,12 +7,11 @@ import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 /**
- * Similar to a PluginLogger, a small Logger extension that prefixes lines with the module name.
+ * Similar to a {@link org.bukkit.plugin.PluginLogger PluginLogger}, a small {@link Logger} extension that prefixes
+ * lines with the module name.
  */
 final class ModuleLogger extends Logger
 {
-
-    private final String prefix;
 
     /**
      * Create a new ModuleLogger that extracts the name from a module.
@@ -21,16 +20,14 @@ final class ModuleLogger extends Logger
      */
     ModuleLogger(@NotNull Module module)
     {
-        super(module.getClass().getCanonicalName(), null);
-        this.prefix = "[" + module.getPlugin().getName() + "/" + module.getName() + "] ";
-        setParent(module.getPlugin().getServer().getLogger());
+        super(module.getPlugin().getName() + "/" + module.getName(), null);
+        setParent(module.getPlugin().getLogger());
         setLevel(Level.ALL);
     }
 
     @Override
     public void log(@NotNull LogRecord logRecord)
     {
-        logRecord.setMessage(prefix + logRecord.getMessage());
         super.log(logRecord);
     }
 
